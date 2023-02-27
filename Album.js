27b515c85b0596c4e4c06eecdee4adebf29e2fdd
jah0766 <joshuahartle@my.unt.edu>
2023-02-27 17:17:25 -0600

    Initial class file creation

---
+++ Album.js
@@ -0,0 +1,13 @@
+//Album Class
+
+class Album
+{
+	constructor(name, artist, albumConvert, songAmount, songs)
+	{
+		this.name = name;
+		this.artist = artist;
+		this.albumCoverart = albumCoverart;
+		this.songAmount = songAmount;
+		this.songs = songs;
+	}
+};
