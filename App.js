250b547c9e3684949443d187bedf10c4ff9a378f
haoxtz <123910289+haoxtz@users.noreply.github.com>
2023-03-25 23:04:17 -0500

    Adding skeleton for future page routing
    Needs more fine tuning, but general idea of what the goal is is outlined.

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import Register from './Register';
+import ImageUpload from './ImageUpload';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-    <main className="App">
-      <Register />
-    </main>
-  );
+    <Router>
+        <Routes>
+            <Upload/>
+            <Route path = "/" exact component = {<Home/>}/>
+            <Route path = "/Register" component = {<Register/>}/>
+        </Routes>
+
+    </Router>
+    );
 }
 
 export default App;
